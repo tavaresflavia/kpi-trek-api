9d51f07679e4cb321858cd99840135e037aa3363
tavaresflavia <altavares.flavia@gmail.com>
2023-10-11 11:52:57 -0400

    fixed authenticate

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,7 @@ router.get('/google', passport.authenticate('google', {
 }));
 
 router.get('/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
+  passport.authenticate('google', { successRedirect: process.env.CLIENT_HOST,  failureRedirect: `${process.env.CLIENT_HOST}/login` }),
   function(_req, res) {
     // Successful authentication, redirect to client-side application
     res.redirect(process.env.CLIENT_HOST);
